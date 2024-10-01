ca0ed42629b62b5d6d3437b3c9be8016b5e5a64d
NasalDooor3097 <albertogutierrezgarci@gmail.com>
2024-09-30 21:32:44 -0600

    nueva act

---
+++ public/scripts/asia.js
@@ -0,0 +1,15 @@
+//document.getElementById("Textos").innerHTML =""
+
+const Afganistan = document.getElementById("Afganistan");
+const Arabia = document.getElementById("Arabia")
+
+Afganistan.addEventListener("click", event =>{
+    document.getElementById("Textos").innerHTML = " <a href='#texto'></a> <h2>Lideres</h2> <br> Presidente: Ashraf Ghani Ahmadzai <br> Ministro: Asadullah Khalid <br> <br> <h2>Divisas</h2> <br> Afgani afgano: 1 Afgani afgano = 0.29 Peso mexicano";
+    document.getElementById("Textos").style.visibility="visible"
+});
+
+
+Arabia.addEventListener("click", event =>{
+    document.getElementById("Textos").innerHTML = "<a href='#texto'></a>  <h2>Lideres</h2> <br> Presidente: Mohamed bin Salman <br> Ministro: Mohamed bin Salman <br> <br> <h2>Divisas</h2> <br> 1 Riyal saudí = 5.24 Peso mexicano";
+    document.getElementById("Textos").style.visibility="visible"
+});
