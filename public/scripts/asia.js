4fae42d4e7e743e78d4b86106aacf9bbeb5796ee
NasalDoor3097 <albertogutierrezgarci@gmail.com>
2024-10-02 09:13:30 -0600

    nueva actualizacion

--- public/scripts/asia.js
+++ public/scripts/asia.js
@@ -4,12 +4,12 @@ const Afganistan = document.getElementById("Afganistan");
 const Arabia = document.getElementById("Arabia")
 
 Afganistan.addEventListener("click", event =>{
-    document.getElementById("Textos").innerHTML = " <a href='#texto'></a> <h2>Lideres</h2> <br> Presidente: Ashraf Ghani Ahmadzai <br> Ministro: Asadullah Khalid <br> <br> <h2>Divisas</h2> <br> Afgani afgano: 1 Afgani afgano = 0.29 Peso mexicano";
+    document.getElementById("Textos").innerHTML = "<h2>Lideres</h2> <br> Presidente: Ashraf Ghani Ahmadzai <br> Ministro: Asadullah Khalid <br> <br> <h2>Divisas</h2> <br> Afgani afgano: 1 Afgani afgano = 0.29 Peso mexicano";
     document.getElementById("Textos").style.visibility="visible"
 });
 
 
 Arabia.addEventListener("click", event =>{
-    document.getElementById("Textos").innerHTML = "<a href='#texto'></a>  <h2>Lideres</h2> <br> Presidente: Mohamed bin Salman <br> Ministro: Mohamed bin Salman <br> <br> <h2>Divisas</h2> <br> 1 Riyal saudí = 5.24 Peso mexicano";
+    document.getElementById("Textos").innerHTML = "<h2>Lideres</h2> <br> Presidente: Mohamed bin Salman <br> Ministro: Mohamed bin Salman <br> <br> <h2>Divisas</h2> <br> 1 Riyal saudí = 5.24 Peso mexicano";
     document.getElementById("Textos").style.visibility="visible"
 });
