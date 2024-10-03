42278b908bd550b4d6e158fe5382bb7b1b30a112
NasalDooor3097 <albertogutierrezgarci@gmail.com>
2024-10-02 23:22:28 -0600

    Ultima Actualizacion 3/Agosto a las 12 :)

--- public/scripts/asia.js
+++ public/scripts/asia.js
@@ -1,4 +1,10 @@
-//document.getElementById("Textos").innerHTML =""
+//Boton para quitar el texto -----------------------------------------------------------
+const ButtonDelete = document.getElementById("delete");
+
+ButtonDelete.addEventListener("click", event =>{
+    document.getElementById("Textos").style.visibility="hidden"
+})
+//fin de la funcion --------------------------------------------------------------------
 
 const Afganistan = document.getElementById("Afganistan");
 const Arabia = document.getElementById("Arabia");
---
+++ public/scripts/europa.js
@@ -0,0 +1,7 @@
+//Boton para quitar el texto -----------------------------------------------------------
+const ButtonDelete = document.getElementById("delete");
+
+ButtonDelete.addEventListener("click", event =>{
+    document.getElementById("Textos").style.visibility="hidden"
+})
+//fin de la funcion --------------------------------------------------------------------
